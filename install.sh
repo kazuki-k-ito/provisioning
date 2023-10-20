c19e3d49eda083f4d1dd6574e095c08684538580
kazuki-k-ito <8922239+kazuki-k-ito@users.noreply.github.com>
2023-10-20 23:07:16 +0900

    :tada: Initial commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## command line tools
+xcode-select --install
+
+## install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## install ansible
+brew install ansible
+
+## install zsh
+brew install zsh
+sudo echo /usr/local/bin/zsh >> /etc/shells
+chsh -s /usr/local/bin/zsh
+
+## install git
+brew install git
+
+## clone this repository
+git clone https://extra-ghe.dev.gree-dev.net/kazuki-k-ito/provisioning.git
+
+## run ansible
+cd provisioning
+ansible-playbook playbook.yaml
